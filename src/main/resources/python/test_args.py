a6191ff24ce366c105eb3f998f2806af0f011d03
Brian Ritchie <brian.ritchie@stfc.ac.uk>
2016-06-27 17:28:15 +0100

    Add more option types
    Added examples of most forms of job options: boolean, boolean group,
    enumeration, integer, float, to test_args.py (which reports their
    values, if any). Added new job_type test_options.xml that uses these.

--- src/main/resources/python/test_args.py
+++ src/main/resources/python/test_args.py
@@ -4,7 +4,8 @@
 # Essentially, just reports whatever it finds on the command line
 # usage: test_args options
 # (mandatory) options: --sessionId=<sessionId> --icatUrl=<icatUrl> --idsUrl=<idsUrl>
-# optional options: --datasetIds=<id1,...> --datafileIds=<id1,...>
+# optional options: --datasetIds=<id1,...> --datafileIds=<id1,...> (though normally at least one would be defined)
+# Further options: --option1=<string> --option2=<string>
 
 import sys
 import os
@@ -22,11 +23,25 @@ parser = IjpOptionParser(usage)
 # Options specific to this script
 # Job Type specs can choose to use these, but can't invent their own
 # (parse_args() will throw an error for unknown options)
+# NOTE: most options specified here only have a long-form,
+# so must always appear on the command-line as --option-name=value or --option-name value,
+# even for boolean flags. '--silent --viewtype blah' will set 'silent' to '--viewtype',
+# and leave 'blah' as an argument instead of as the value of the viewtype option.
 
 parser.add_option("--option-one", dest="optionOne",
                  help="script-specific option 1")
 parser.add_option("--option-two", dest="optionTwo",
                  help="script-specific option 2")
+parser.add_option("--mode", dest="mode",
+                  help="plain, verbose or effusive")
+parser.add_option("--silent", dest="silent",
+                  help="keep quiet, regardless of --mode")
+parser.add_option("--viewtype", dest="viewtype",
+                  help="View type (fullframe, beads, etc.)")
+parser.add_option("--index", dest="index",
+                  help="Initial index (0-10, default 1)")
+parser.add_option("--origin", dest="origin",
+                  help="Origin (range [-2.0,2.0]")
 
 (options, args) = parser.parse_args()
 
@@ -67,6 +82,25 @@ if options.optionOne:
 
 if options.optionTwo:
     print "Option 2 = ", options.optionTwo
+
+if options.mode:
+    print "Mode = ", options.mode
+
+if options.silent:
+    # Note: print the value of silent; we expect a boolean,
+    # but this helps debug misuses such as 'test_args --silent --viewtype whitebeam'
+    print "Silent = ", options.silent
+else:
+    print "Silent = false"
+    
+if options.viewtype:
+    print "View type = ", options.viewtype
+    
+if options.index:
+    print "Index = ", options.index
+
+if options.origin:
+    print "Origin = ", options.origin
 
 print "Other command-line arguments (if any): ", args
 
