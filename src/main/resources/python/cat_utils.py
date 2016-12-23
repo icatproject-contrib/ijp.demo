78ce9dd8a48bf11f5135f6c62b38a44983dc22d3
Brian Ritchie <brian.ritchie@stfc.ac.uk>
2016-12-23 14:23:01 +0000

    Replace OptionParser with ArgumentParser
    Changed IjpOptionParser to inherit from ArgumentParser rather than
    OptionParser. Added add_option() and overrode parse_args() so that
    existing scripts should continue to work unmodified.

--- src/main/resources/python/cat_utils.py
+++ src/main/resources/python/cat_utils.py
@@ -11,7 +11,7 @@ import os
 from urllib import urlencode
 import ids
 import tempfile
-from optparse import OptionParser
+from argparse import ArgumentParser
 
 def terminate(msg, status):
     """
@@ -207,9 +207,9 @@ class Tee(threading.Thread):
             for out in self.out:
                 out.write(line)
 
-class IjpOptionParser(OptionParser):
+class IjpOptionParser(ArgumentParser):
     """
-    OptionParser that predefines some standard IJP options:
+    ArgumentParser that predefines some standard IJP options:
       --sessionId  - the ICAT session ID
       --icatUrl     - URL for the ICAT service
       --idsUrl      - URL for the IDS service
@@ -220,28 +220,59 @@ class IjpOptionParser(OptionParser):
     The IJP will pass these options to job scripts if the job type
     specifies that the job will expect them.  The option dest names
     match their names, e.g.:
-    
+
       usage = "usage: %prog options"
-      parser = IjpOptions(usage)
+      parser = IjpOptionParser(usage)
       (options,args) = parser.parse_args()
       if options.datasetIds:
           datasetIdsList = options.datasetIds.split(',')
           ...
+    Note: for compatibility with pre-existing jobscripts,
+    this class is (still) called IjpOptionParser; and it
+    provides a mapping from add_option to add_argument.
+    New scripts should use add_argument.
     """
     def __init__(self,usage):
-        OptionParser.__init__(self,usage)
-        self.add_option("--sessionId", dest="sessionId",
+        ArgumentParser.__init__(self,usage)
+        self.add_argument("--sessionId", dest="sessionId",
                       help="ICAT session ID")
-        self.add_option("--icatUrl", dest="icatUrl",
+        self.add_argument("--icatUrl", dest="icatUrl",
                       help="ICAT url")
-        self.add_option("--idsUrl", dest="idsUrl",
+        self.add_argument("--idsUrl", dest="idsUrl",
                       help="IDS url")
-        self.add_option("--ijpUrl", dest="ijpUrl",
+        self.add_argument("--ijpUrl", dest="ijpUrl",
                       help="IJP url")
-        self.add_option("--ijpJobId", dest="ijpJobId",
+        self.add_argument("--ijpJobId", dest="ijpJobId",
                       help="the job ID used in ijp")
-        self.add_option("--datasetIds", dest="datasetIds",
+        self.add_argument("--datasetIds", dest="datasetIds",
                      help="comma-separated dataset IDs")
-        self.add_option("--datafileIds", dest="datafileIds",
+        self.add_argument("--datafileIds", dest="datafileIds",
                      help="comma-separated datafile IDs")
-
+    def add_option(self,*vargs,**kwargs):
+      """
+      Map existing add_option uses to add_argument
+      """
+      # Convert type from string to type
+      if 'type' in kwargs:
+          if kwargs['type'] is None or kwargs['type'] == 'string':
+              kwargs['type'] = None
+          elif kwargs['type'] == 'int':
+              kwargs['type'] = int
+          elif kwargs['type'] == 'float':
+              kwargs['type'] = float
+      self.add_argument(*vargs,**kwargs)
+    def parse_args(self,args=None, namespace=None):
+        """
+        Return a pair (options,args) so pre-existing scripts
+        will still work.  This is a fudge! The 'options' value
+        will also contain the positional arguments; but old
+        scripts won't look at them.
+        New scripts will have to accept a paired result,
+        but can ignore the second value.
+        """
+        args = super(IjpOptionParser,self).parse_args(args,namespace)
+        if 'args' in args:
+            newArgs = args.args
+        else:
+            newArgs = []
+        return (args,newArgs)
